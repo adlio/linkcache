9b9773c9f88a6d472c0940cf6dd2d6bf371d6b07
Aaron Longwell <a@adl.io>
2024-08-25 21:03:52 -0700

    Add simple tests

--- tests/cache_tests.rs
+++ tests/cache_tests.rs
@@ -1,36 +1,36 @@
+use std::{fs::Permissions, os::unix::fs::PermissionsExt};
+
 use linkcache::*;
 use tempfile::*;
 
 /// Creates a Cache instance in a randomly-named temporary directory so
 /// that subsequent test runs are isolated from one another
-fn test_cache_instance() -> Cache {
+fn test_cache_instance() -> (Cache, TempDir) {
     let binding = tempdir().expect("Failed to create temp dir");
-    CacheBuilder::new()
-        .with_root_dir(binding.into_path())
-        .build()
-        .expect("Failed to build cache instance")
+    let dir = binding.path();
+    std::fs::set_permissions(binding.path(), Permissions::from_mode(0o755))
+        .expect("Failed to set directory permissions");
+    let file_path = dir.join("test.sqlite");
+    println!("Using cache file: {:?}", file_path);
+    let cache = Cache::new(file_path).expect("Failed to create test cache");
+    (cache, binding)
 }
 
 #[test]
-fn test_indexing_chrome_bookmarks() {
-    let mut cache = test_cache_instance();
+fn test_indexing_chrome_bookmarks() -> Result<()> {
+    let (mut cache, _temp_dir) = test_cache_instance();
 
-    assert!(cache
-        .add(Link {
-            title: "Visual Studio Code".to_string(),
-            url: "https://code.visualstudio.com".to_string(),
-            ..Default::default()
-        })
-        .is_ok());
-    assert!(cache
-        .add(Link {
-            title: "Sublime Text".to_string(),
-            url: "https://www.sublimetext.com".to_string(),
-            ..Default::default()
-        })
-        .is_ok());
-    cache.commit().expect("Failed to commit");
-    let results = &cache.search("Viz Studio").expect("Search failed");
+    cache.add(Link {
+        title: "Visual Studio Code".to_string(),
+        url: "https://code.visualstudio.com".to_string(),
+        ..Default::default()
+    })?;
+    cache.add(Link {
+        title: "Sublime Text".to_string(),
+        url: "https://www.sublimetext.com".to_string(),
+        ..Default::default()
+    })?;
+    let results = &cache.search("VS Code").expect("Search failed");
     assert!(!results.is_empty());
     assert_eq!(results[0].title, "Visual Studio Code");
 
@@ -40,4 +40,5 @@ fn test_indexing_chrome_bookmarks() {
     browser
         .cache_bookmarks(&mut cache)
         .expect("Failed to cache bookmarks");
+    Ok(())
 }
