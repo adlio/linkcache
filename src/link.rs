d67a4b428c0ff97e2bcd9933f72302bee0cac747
Aaron Longwell <a@adl.io>
2024-08-25 20:56:10 -0700

    Simplified Link type

--- src/link.rs
+++ src/link.rs
@@ -1,35 +1,30 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Link {
-    // Required fields
     pub url: String,
+
     pub title: String,
-    /// Timestamp as a number of seconds since the epoch
-    /// assumed to be UTC, but we assume consumers will
-    /// handle time zone conversion.
-    pub timestamp: i64,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub subtitle: Option<String>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub author: Option<String>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub id: Option<usize>,
+    pub source: Option<String>,
+
+    pub timestamp: DateTime<Utc>,
+
     #[serde(skip_serializing_if = "Option::is_none")]
     pub score: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub subtitle: Option<String>,
-
-    /// Customization of the short and long title options
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub short_title: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub long_title: Option<String>,
 }
 
 impl Link {
     pub fn new(url: String, title: String) -> Link {
-        let timestamp = chrono::Utc::now().timestamp();
+        let timestamp = chrono::Utc::now();
         Link {
             url,
             title,
@@ -43,8 +38,9 @@ impl Link {
         self
     }
 
-    pub fn with_timestamp(mut self, timestamp: i64) -> Self {
-        self.timestamp = timestamp;
+    pub fn with_timestamp_seconds(mut self, timestamp_seconds: i64) -> Self {
+        let timestamp = DateTime::from_timestamp(timestamp_seconds, 0);
+        self.timestamp = timestamp.expect("Failed to create timestamp");
         self
     }
 
