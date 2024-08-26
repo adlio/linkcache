17e8d4cdaf52912078fc11734e043d7b3dd8ae1f
Aaron Longwell <a@adl.io>
2024-08-25 20:56:33 -0700

    Simplify Cache after removing Tantivy

--- src/cache.rs
+++ src/cache.rs
@@ -1,135 +1,71 @@
-use std::{fs, path::PathBuf};
-
-use serde_json::{from_str, to_string};
-use tantivy::collector::TopDocs;
-use tantivy::query::{AllQuery, QueryParser};
-use tantivy::schema::*;
-use tantivy::tokenizer::*;
-use tantivy::{doc, Index, IndexWriter};
+use rusqlite::{Connection, OpenFlags};
+use std::path::{Path, PathBuf};
 
 use crate::{error::Result, Link};
 
-const TANTIVY_INDEX_DIR: &str = ".tantivy";
-const TANTIVY_WRITER_SIZE: usize = 256_000_000; // 256 MB
-
-pub struct CacheBuilder {
-    root_dir: PathBuf,
-}
-
-/// Cache wraps an instance of the link cache. Each instance has a root
-/// directory that contains data files in both a raw format and a
-/// Tantivy index format.
 pub struct Cache {
-    index: Index,
-    writer: IndexWriter,
-}
-
-impl CacheBuilder {
-    pub fn new() -> Self {
-        CacheBuilder {
-            root_dir: Cache::default_root_dir(),
-        }
-    }
-
-    pub fn with_root_dir(&mut self, dir: PathBuf) -> &mut Self {
-        self.root_dir = dir;
-        self
-    }
-
-    /// Initializes the index, its schema, and custom tokenization
-    pub fn build(&self) -> Result<Cache> {
-        // Ensure the index directory exists
-        let index_path = self.root_dir.join(TANTIVY_INDEX_DIR);
-        fs::create_dir_all(&index_path)?;
-
-        // Schema
-        let mut builder = Schema::builder();
-        builder.add_text_field(
-            "title",
-            TextOptions::default().set_indexing_options(
-                TextFieldIndexing::default()
-                    .set_tokenizer("ngram")
-                    .set_index_option(IndexRecordOption::WithFreqsAndPositions),
-            ),
-        );
-        builder.add_text_field(
-            "author",
-            TextOptions::default().set_indexing_options(
-                TextFieldIndexing::default()
-                    .set_tokenizer("ngram")
-                    .set_index_option(IndexRecordOption::WithFreqsAndPositions),
-            ),
-        );
-        builder.add_text_field("subtitle", TEXT);
-        builder.add_text_field("url", STRING);
-        builder.add_date_field("timestamp", STORED | FAST);
-        builder.add_text_field("struct_json", STORED);
-        let schema = builder.build();
-
-        // Open or create the index
-        let index = Index::open_in_dir(&index_path)
-            .or_else(|_| Index::create_in_dir(&index_path, schema))?;
-
-        // Custom NGram Tokenizer for the Title field
-        let tokenizer = TextAnalyzer::builder(NgramTokenizer::new(3, 3, false).unwrap())
-            .filter(LowerCaser)
-            .filter(StopWordFilter::remove(vec![
-                "to".to_string(),
-                "the".to_string(),
-                "and".to_string(),
-            ]))
-            .build();
-        index.tokenizers().register("ngram", tokenizer);
-
-        // Prepare an index writer
-        let writer = index.writer(TANTIVY_WRITER_SIZE)?;
-        Ok(Cache { index, writer })
-    }
-}
-
-impl Default for CacheBuilder {
-    fn default() -> Self {
-        Self::new()
-    }
+    pub(crate) conn: Connection,
 }
 
 impl Cache {
+    /// Create a new Cache instance with the SQLite database at the provided
+    /// path. This could fail if the path doesn't exist, or the file isn't
+    /// writeable, or the initialization process (creation of tables,
+    /// triggers, etc) fails.
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let conn = Connection::open_with_flags(
+            path,
+            OpenFlags::SQLITE_OPEN_READ_WRITE | OpenFlags::SQLITE_OPEN_CREATE,
+        )?;
+        let cache = Cache { conn };
+        cache.initialize()?;
+        Ok(cache)
+    }
+
+    pub fn default() -> Result<Self> {
+        let cache_dir = dirs::home_dir()
+            .unwrap_or_else(|| PathBuf::from("/tmp"))
+            .join(".linkcache");
+        std::fs::create_dir_all(&cache_dir)?;
+        let db_path = cache_dir.join("linkcache.sqlite");
+        Self::new(db_path)
+    }
+
     /// Adds a new link to the index. The url field is used as the unique
     /// key. This function removes any existing link with the same url before
     /// saving a new one. The commit() function must be called after adding
     /// to persist the changes. Batch updates should call add() many times
     /// and commit() once.
     pub fn add(&mut self, link: Link) -> Result<()> {
-        self.remove(&link)?;
+        // let json_str = to_string(&link)?;
 
-        let json_str = to_string(&link)?;
-
-        let writer = &self.writer;
-        writer.add_document(doc!(
-            self.index.schema().get_field("title")? => link.title,
-            self.index.schema().get_field("subtitle")? => link.subtitle.unwrap_or_default(),
-            self.index.schema().get_field("url")? => link.url,
-            self.index.schema().get_field("author")? => link.author.unwrap_or_default(),
-            self.index.schema().get_field("timestamp")? => link.timestamp,
-            self.index.schema().get_field("struct_json")? => json_str,
-        ))?;
+        self.conn.execute(
+            "INSERT OR REPLACE INTO links (
+                url, title, subtitle,
+                source, author,
+                timestamp
+            ) VALUES (
+                ?1, ?2, ?3,
+                ?4, ?5,
+                ?6
+            )",
+            (
+                &link.url,
+                &link.title,
+                &link.subtitle,
+                &link.source,
+                &link.author,
+                &link.timestamp,
+            ),
+        )?;
         Ok(())
     }
 
     /// Removes a Link from the index. The url field is used as the unique key.
-    /// The commit() function must be called after removing to persist the
-    /// the change on disk.
     pub fn remove(&mut self, link: &Link) -> Result<()> {
-        let field = self.index.schema().get_field("url")?;
-        let term = Term::from_field_text(field, &link.url.to_string());
-        let _ = self.writer.delete_term(term);
-        Ok(())
-    }
+        self.conn
+            .execute("DELETE FROM links WHERE url = ?1", [&link.url])?;
 
-    /// Persists all pending index changes to disk. No change is reflected in
-    /// the index until this is called.
-    pub fn commit(&mut self) -> Result<()> {
-        self.writer.commit()?;
         Ok(())
     }
 
@@ -139,126 +75,91 @@ impl Cache {
             return self.get_latest_n(50);
         }
 
-        let reader = self.index.reader()?;
-        let searcher = reader.searcher();
-
-        let title_field = self.index.schema().get_field("title").unwrap();
-        let subtitle_field = self.index.schema().get_field("subtitle").unwrap();
-        let mut parser = QueryParser::for_index(&self.index, vec![title_field, subtitle_field]);
-        parser.set_field_fuzzy(title_field, true, 1, true);
-        parser.set_field_boost(title_field, 2.0);
-        let query = parser.parse_query_lenient(query).0;
-
-        let top_docs = searcher.search(&query, &TopDocs::with_limit(20))?;
-
-        let results = top_docs
-            .into_iter()
-            .map(|(score, doc_address)| {
-                let doc: TantivyDocument = searcher.doc(doc_address).unwrap();
-                let mut link = self.doc_to_link(&doc);
-                link.score = Some(score);
-                link
-            })
-            .collect();
-        Ok(results)
-    }
-
-    pub fn get_latest_n(&self, n: usize) -> Result<Vec<Link>> {
-        let reader = self.index.reader()?;
-        let searcher = reader.searcher();
-
-        let top_docs = searcher.search(
-            &AllQuery,
-            &TopDocs::with_limit(n).order_by_fast_field("timestamp", tantivy::Order::Desc),
+        let mut stmt = self.conn.prepare(
+            "SELECT links.* FROM links_fts
+             JOIN links ON links_fts.url = links.url
+             WHERE links_fts MATCH ?1
+             ORDER BY rank",
         )?;
-        let results = top_docs
-            .into_iter()
-            .map(
-                |(_timestamp, doc_address): (tantivy::DateTime, tantivy::DocAddress)| {
-                    let doc: TantivyDocument = searcher.doc(doc_address).unwrap();
-                    self.doc_to_link(&doc)
-                },
-            )
-            .collect();
-        Ok(results)
+
+        let links_iter = stmt.query_map([query], |row| {
+            Ok(Link {
+                url: row.get(0)?,
+                title: row.get(1)?,
+                subtitle: row.get(2)?,
+                source: row.get(3)?,
+                author: row.get(4)?,
+                timestamp: row.get(5)?,
+                ..Default::default()
+            })
+        })?;
+
+        links_iter
+            .collect::<std::result::Result<Vec<_>, rusqlite::Error>>()
+            .map_err(|e| e.into())
     }
 
-    /// Calculates the default root directory for a cache. Will use the
-    /// .linkcache directory in the user's home directory, or /tmp/.linkcache
-    /// as an alternate if we don't have a home directory.
-    fn default_root_dir() -> PathBuf {
-        dirs::home_dir()
-            .unwrap_or_else(|| PathBuf::from("/tmp"))
-            .join(".linkcache")
-    }
+    pub fn get_latest_n(&self, n: u32) -> Result<Vec<Link>> {
+        let mut stmt = self.conn.prepare(
+            "SELECT url, title, subtitle, source, author, timestamp 
+             FROM links 
+             ORDER BY timestamp DESC 
+             LIMIT ?",
+        )?;
 
-    /// Converts a Tantivy document into a Link struct by parsing the
-    /// struct_json field.
-    ///
-    fn doc_to_link(&self, doc: &TantivyDocument) -> Link {
-        let json_string = doc
-            .get_first(self.index.schema().get_field("struct_json").unwrap())
-            .and_then(|v| v.as_str())
-            .unwrap()
-            .to_owned();
-        let mut link: Link = from_str(json_string.as_str()).unwrap();
-        let date = chrono::DateTime::from_timestamp(link.timestamp, 0)
-            .unwrap()
-            .format("%Y-%m-%d %I:%M:%S %p")
-            .to_string();
-        link.subtitle = Some(date);
-        link
+        let links_iter = stmt.query_map([n], |row| {
+            Ok(Link {
+                url: row.get(0)?,
+                title: row.get(1)?,
+                subtitle: row.get(2)?,
+                source: row.get(3)?,
+                author: row.get(4)?,
+                timestamp: row.get(5)?,
+                ..Default::default()
+            })
+        })?;
+
+        links_iter
+            .collect::<std::result::Result<Vec<_>, rusqlite::Error>>()
+            .map_err(|e| e.into())
     }
 }
 
 /// Defines the Default implementaton for Cache.
 impl Default for Cache {
     fn default() -> Self {
-        CacheBuilder::new().build().unwrap()
+        Self::default().expect("Failed to create default cache")
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tempfile::tempdir;
+    use tempfile::{tempdir, TempDir};
 
-    fn test_cache_instance() -> Cache {
+    fn test_cache_instance() -> (Cache, TempDir) {
         let binding = tempdir().expect("Failed to create temp dir");
         let temp_dir = binding.path();
-        let cache = CacheBuilder::new()
-            .with_root_dir(temp_dir.to_path_buf())
-            .build()
-            .expect("Failed to build cache");
-        cache
+        let cache = Cache::new(temp_dir.join("test.sqlite")).expect("Failed to create test cache");
+        (cache, binding)
     }
 
     #[test]
-    fn test_new_default_path() {
-        let cache = CacheBuilder::new();
-        assert!(cache.root_dir.ends_with(".linkcache"));
-    }
-
-    #[test]
-    fn test_add_and_search_fuzzy() {
-        let mut cache = test_cache_instance();
-        assert!(cache
-            .add(Link {
-                title: "Visual Studio Code".to_string(),
-                url: "https://code.visualstudio.com".to_string(),
-                ..Default::default()
-            })
-            .is_ok());
-        assert!(cache
-            .add(Link {
-                title: "Sublime Text".to_string(),
-                url: "https://www.sublimetext.com".to_string(),
-                ..Default::default()
-            })
-            .is_ok());
-        assert!(cache.commit().is_ok());
-        let results = cache.search("Viz Studio").expect("Search failed");
+    fn test_add_and_search_fuzzy() -> Result<()> {
+        let (mut cache, _temp_dir) = test_cache_instance();
+        cache.add(Link {
+            title: "Visual Studio Code".to_string(),
+            url: "https://code.visualstudio.com".to_string(),
+            ..Default::default()
+        })?;
+        cache.add(Link {
+            title: "Sublime Text".to_string(),
+            url: "https://www.sublimetext.com".to_string(),
+            ..Default::default()
+        })?;
+        let results = cache.search("Vis studio")?;
         assert!(!results.is_empty());
         assert_eq!(results[0].title, "Visual Studio Code");
+        Ok(())
     }
 }
